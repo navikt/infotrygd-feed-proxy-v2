29a11478e4d45c53093ba881c464811d1473c4b5
Saikat Halder <45660476+haldersaikat@users.noreply.github.com>
2023-02-15 15:48:27 +0100

    implementerte tjenester for å hente feed fra baks-infotrygd-feed (#3)
    * implementerte tjenester for å hente feed fra baks-infotrygd-feed
    
    * la til konfig slik at infotrygd-feed-konsument applikasjon kan kalle appen
    
    * la til konfig slik at appen kan kalle familie-baks-infotrygd-feed
    
    * la til STS konfigurasjon
    
    * fikset client slik at BAKS_INFOTRYGD_FEED kalles med azure token
    
    * La til STS_URL
    
    * La til Stsvalidator for å validere token
    
    * Fjernet @ProtectedWithClaims
    
    * la til @Protected på både kontrollere
    
    * fikset property
    
    * prøver med ProtectedWithClaims
    
    * fjernet StsValidator.kt

---
+++ src/main/kotlin/no/nav/infotrygd/feed/proxy/integration/BaksInfotrygdFeedClient.kt
@@ -0,0 +1,50 @@
+package no.nav.infotrygd.feed.proxy.integration
+
+import no.nav.familie.http.client.AbstractRestClient
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestOperations
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+
+@Service
+class BaksInfotrygdFeedClient(
+    @Value("\${BAKS_INFOTRYGD_FEED_URL}") private val baksInfotrygdFeedUri: URI,
+    @Qualifier("azure") restOperations: RestOperations
+) : AbstractRestClient(restOperations, "baksInfotrygdFeed") {
+
+    fun hentBarnetrygdFeed(sekvensnummer: Long): String {
+        val hentBarnetrygdFeedUri = UriComponentsBuilder.fromUri(baksInfotrygdFeedUri)
+            .pathSegment("/api/barnetrygd/v1/feed")
+            .queryParam("sistLesteSekvensId", sekvensnummer)
+            .build().toUri()
+        logger.info("Henter barnetrygd feed med URI=$hentBarnetrygdFeedUri")
+        return getForEntity<String>(hentBarnetrygdFeedUri, headers()).also {
+            logger.info("Hentet barnetrygd feed med URI=$hentBarnetrygdFeedUri. Kall ok")
+        }
+    }
+
+    fun hentKontantstøtteFeed(sekvensnummer: Long): String {
+        val hentBarnetrygdFeedUri = UriComponentsBuilder.fromUri(baksInfotrygdFeedUri)
+            .pathSegment("/api/kontantstotte/v1/feed")
+            .queryParam("sistLesteSekvensId", sekvensnummer)
+            .build().toUri()
+        logger.info("Henter kontantstotte feed med URI=$hentBarnetrygdFeedUri")
+        return getForEntity<String>(hentBarnetrygdFeedUri, headers()).also {
+            logger.info("Hentet kontantstotte feed med URI=$hentBarnetrygdFeedUri. Kall ok")
+        }
+    }
+
+    private fun headers(): HttpHeaders = HttpHeaders().apply {
+        contentType = MediaType.APPLICATION_JSON
+        accept = listOf(MediaType.APPLICATION_JSON)
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
+    }
+}
