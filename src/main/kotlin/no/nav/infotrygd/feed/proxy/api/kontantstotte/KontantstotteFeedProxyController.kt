2e829a9aeb81f71645650c435bfdf002be7ec8d1
bengtolav-decisive <bengt.olav.olsen@nav.no>
2024-04-04 15:00:36 +0200

    TREX-2664 Autoformatering av IntelliJ, suppress warnings

--- src/main/kotlin/no/nav/infotrygd/feed/proxy/api/kontantstotte/KontantstotteFeedProxyController.kt
+++ src/main/kotlin/no/nav/infotrygd/feed/proxy/api/kontantstotte/KontantstotteFeedProxyController.kt
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+@Suppress("NonAsciiCharacters")
 @RestController
 @RequestMapping("/api/kontantstotte")
 @ProtectedWithClaims(issuer = "sts")
