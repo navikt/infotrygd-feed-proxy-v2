787fa5e73a5553d30ffab9b440afd3e354a41dcf
Erlend Kristiansen <erlend.kristiansen@nav.no>
2024-06-25 14:24:48 +0200

    TREX-2664 Midlertidig logging av yrkesskade meldinger

--- src/main/kotlin/no/nav/infotrygd/feed/proxy/integration/YrkesskadeFeedClient.kt
+++ src/main/kotlin/no/nav/infotrygd/feed/proxy/integration/YrkesskadeFeedClient.kt
@@ -25,6 +25,7 @@ class YrkesskadeFeedClient(
         logger.info("Henter yrkesskade feed med URI=$hentYrkesskadeFeedUri")
         return getForEntity<String>(hentYrkesskadeFeedUri, headers()).also {
             logger.info("Hentet yrkesskade feed med URI=$hentYrkesskadeFeedUri. Kall ok")
+            logger.info("$it") // TODO erlend 25.22.2024: Fjern logging av dataene før prodsetting
             securelogger.info("Yrkesskade: $it")
         }
     }
