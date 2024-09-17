69baf36bb8bbae24f2c663cd5ce34b6717e6a468
Bengt Olav Olsen <89935165+bengtolav-decisive@users.noreply.github.com>
2024-09-17 10:54:22 +0200

    Trex 2784 foreldrepenger (#5)
    * TREX-2664 Autoformatering av IntelliJ, suppress warnings
    
    * TREX-2664 Refaktorering av navn
    
    * TREX-2664 Yrkesskade initiell commit
    
    * TREX-2664 Autoformatering av IntelliJ, suppress warnings
    
    * TREX-2664 Refaktorering av navn
    
    * TREX-2664 rettet konstanter i application-prod.yaml
    
    * TREX-2664 rettet resource url ++
    
    * TREX-2664 rettet resource url + liten refactor
    
    * TREX-2664 Legg til dependency til flyway og postgres
    
    * TREX-2664 Fjern dependency til flyway og postgres
    
    * TREX-2784 foreldre og svangerskap + liten refactor
    
    * TREX-2784 fpsak application resource
    
    * TREX-2784 changed build script
    
    * TREX-2784 bumped script versions
    
    * TREX-2784 corrected environment prod
    
    * TREX-2784 dependabot upgrade
    
    * TREX-2784 correcting fp resource url
    
    ---------
    
    Co-authored-by: Erlend Kristiansen <erlend.kristiansen@nav.no>

---
+++ src/main/kotlin/no/nav/infotrygd/feed/proxy/api/foreldrepenger/ForeldrepengerFeedProxyController.kt
@@ -0,0 +1,49 @@
+package no.nav.infotrygd.feed.proxy.api.foreldrepenger
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import no.nav.infotrygd.feed.proxy.integration.ForeldreOgSvangerskapspengerFeedClient
+import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/fpsak/api/feed")
+@ProtectedWithClaims(issuer = "sts")
+class ForeldrepengerFeedProxyController(private val foreldrepengerFeedClient: ForeldreOgSvangerskapspengerFeedClient) {
+
+    @Operation(
+        summary = "Hent liste med hendelser.",
+        description = "Henter hendelser med sekvensId større enn sistLesteSekvensId.",
+    )
+    @GetMapping("/vedtak/foreldrepenger", produces = ["application/json; charset=us-ascii"])
+    fun hentFeed(
+        @Parameter(description = "Sist leste sekvensnummer.", required = true, example = "0")
+        @RequestParam("sistLesteSekvensId")
+        sekvensnummer: Long,
+    ): ResponseEntity<String> {
+        return Result.runCatching {
+            foreldrepengerFeedClient.hentForeldrepengerFeed(sekvensnummer = sekvensnummer)
+        }.fold(
+            onSuccess = { feed ->
+                logger.info("Hentet feeds fra sekvensnummer $sekvensnummer")
+                secureLogger.info("Hentet feeds $feed fra sekvensnummer $sekvensnummer")
+                ResponseEntity.ok(feed)
+            },
+            onFailure = {
+                logger.error("Feil ved henting av feeds fra sekvensnummer $sekvensnummer", it)
+                ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+            },
+        )
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
+        private val secureLogger = LoggerFactory.getLogger("secureLogger")
+    }
+}
