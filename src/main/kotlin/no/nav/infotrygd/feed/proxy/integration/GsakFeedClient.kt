19cead05277806e7ba38ef3a74e7562ba44545cb
Bengt Olav Olsen <bengt.olav.olsen@decisive.no>
2025-09-26 15:41:01 +0200

    TREX-2924 - Nye klasser for å opprette/ferdigstille saker i gsak

---
+++ src/main/kotlin/no/nav/infotrygd/feed/proxy/integration/GsakFeedClient.kt
@@ -0,0 +1,69 @@
+package no.nav.infotrygd.feed.proxy.integration
+
+import no.nav.infotrygd.feed.proxy.integration.http.klient.AbstractRestClient
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestOperations
+import org.springframework.web.util.UriComponentsBuilder
+import java.net.URI
+import java.util.UUID
+
+@Service
+class GsakFeedClient (
+    @Value("\${GSAK_URL}") private val gsakUri: URI,
+    @Qualifier("azureCC") restOperations: RestOperations,
+) : AbstractRestClient(restOperations) {
+
+    fun opprettOppgave(brukerident: String, tildeltEnhetsnr: String, opprettetAvEnhetsnr: String,
+                       beskrivelse: String, oppgavetype: String): String {
+        val prioritet ="HOY"
+        val opprettOppgaveUri =
+            UriComponentsBuilder
+                .fromUri(gsakUri)
+                .pathSegment("api/v1/oppgaver")
+                .build().toUri()
+        logger.info("Oppretter oppgave med URI=$opprettOppgaveUri")
+        return postForEntity<String, gsakOpprettOppgaveRequest>(opprettOppgaveUri, headers(),
+            gsakOpprettOppgaveRequest(brukerident, tildeltEnhetsnr, opprettetAvEnhetsnr,
+                beskrivelse, oppgavetype, prioritet)).also {
+            logger.info("Opprettet oppgave med URI=$opprettOppgaveUri. Kall ok.")
+        }
+    }
+
+    fun ferdigstillOppgave(oppgaveId: Long, tekst: String): String {
+        val status = "FERDIGSTILT"
+        val ferdigstillOppgaveUri =
+            UriComponentsBuilder
+                .fromUri(gsakUri)
+                .pathSegment("api/v1/oppgaver/" + oppgaveId)
+                .build().toUri()
+        logger.info("Ferdigstiller oppgave med URI=$ferdigstillOppgaveUri")
+        return patchForEntity<String, GsakFerdigstillOppgaveRequest>(ferdigstillOppgaveUri, headers(),
+            GsakFerdigstillOppgaveRequest(status,
+                GsakFerdigstillOppgaveKommentar(tekst))).also {
+            logger.info("Ferdigstilt oppgave med URI=$ferdigstillOppgaveUri. Kall ok.")
+        }
+    }
+
+    private fun headers(): HttpHeaders = HttpHeaders().apply {
+        contentType = MediaType.APPLICATION_JSON
+        accept = listOf(MediaType.APPLICATION_JSON)
+        set("X-Correlation-ID", UUID.randomUUID().toString())
+    }
+
+    data class gsakOpprettOppgaveRequest(val brukerident: String, val tildeltEnhetsnr: String,
+                                         val opprettetAvEnhetsnr: String, val beskrivelse: String,
+                                         val oppgavetype: String, val prioritet: String)
+
+    data class GsakFerdigstillOppgaveRequest(val status: String, val kommentar: GsakFerdigstillOppgaveKommentar)
+
+    data class GsakFerdigstillOppgaveKommentar(val tekst: String)
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
+    }
+}
