90c7491061e6772108b2e159d0711e4ff1521214
bengtolav-decisive <bengt.olav.olsen@nav.no>
2026-01-26 15:00:58 +0100

    TREX-2924: Endret til PostMapping

--- src/main/kotlin/no/nav/infotrygd/feed/proxy/api/oppgave/OppgaveProxyController.kt
+++ src/main/kotlin/no/nav/infotrygd/feed/proxy/api/oppgave/OppgaveProxyController.kt
@@ -6,7 +6,6 @@ import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -51,7 +50,7 @@ class OppgaveProxyController(
         summary = "Ferdigstill oppgave.",
         description = "Ferdigstiller en oppgave.",
     )
-    @GetMapping("/v1/ferdigstill", produces = ["application/json; charset=us-ascii"])
+    @PostMapping("/v1/ferdigstill", produces = ["application/json; charset=us-ascii"])
     fun ferdigstillOppgave(
         @RequestBody ferdigstillBody: FerdigstillOppgaveBody
     ): ResponseEntity<String> =
@@ -77,7 +76,7 @@ class OppgaveProxyController(
         summary = "Ferdigstill oppgave.",
         description = "Ferdigstiller en oppgave.",
     )
-    @GetMapping("/v1/ferdigstill/uk/", produces = ["application/json; charset=us-ascii"])
+    @PostMapping("/v1/ferdigstill/uk/", produces = ["application/json; charset=us-ascii"])
     fun ferdigstillOppgaveUk(
         @RequestBody ferdigstillBody: FerdigstillOppgaveUkBody
     ): ResponseEntity<String> =
