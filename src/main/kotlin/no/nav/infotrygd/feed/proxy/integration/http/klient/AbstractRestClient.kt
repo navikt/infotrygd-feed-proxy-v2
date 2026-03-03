6dd0a46ef64a06efcd32945f9d94caea22a53f49
bengtolav-decisive <bengt.olav.olsen@nav.no>
2026-03-03 12:35:16 +0100

    Fjernet secureLogger fra flere steder

--- src/main/kotlin/no/nav/infotrygd/feed/proxy/integration/http/klient/AbstractRestClient.kt
+++ src/main/kotlin/no/nav/infotrygd/feed/proxy/integration/http/klient/AbstractRestClient.kt
@@ -17,7 +17,6 @@ import java.net.URI
 abstract class AbstractRestClient(
     val operations: RestOperations,
 ) {
-    private val secureLogger: Logger = LoggerFactory.getLogger("secureLogger")
     private val log: Logger = LoggerFactory.getLogger(this::class.java)
 
     inline fun <reified T : Any> getForEntity(
@@ -46,7 +45,6 @@ abstract class AbstractRestClient(
         uri: URI,
     ): T {
         if (!respons.statusCode.is2xxSuccessful) {
-            secureLogger.info("Kall mot $uri feilet:  ${respons.body}")
             log.info("Kall mot $uri feilet: ${respons.statusCode}")
             throw HttpServerErrorException(respons.statusCode, "",
                 respons.body?.toString()?.toByteArray(), Charsets.UTF_8)
@@ -62,13 +60,10 @@ abstract class AbstractRestClient(
             val responseEntity = function.invoke()
             return validerOgPakkUt(responseEntity, uri)
         } catch (e: RestClientResponseException) {
-            secureLogger.warn("RestClientResponseException ved kall mot uri=$uri", e)
             lesRessurs(e)?.let { throw RessursException(it, e) } ?: throw e
         } catch (e: HttpClientErrorException) {
-            secureLogger.warn("HttpClientErrorException ved kall mot uri=$uri", e)
             lesRessurs(e)?.let { throw RessursException(it, e) } ?: throw e
         } catch (e: Exception) {
-            secureLogger.warn("Feil ved kall mot uri=$uri", e)
             throw RuntimeException("Feil ved kall mot uri=$uri", e)
         }
     }
