68f8e32836d92e25554163c4ed8034ba5de9341d
Bjørn Erik Sannes <bjorn.erik.sannes@nav.no>
2026-09-16 11:24:11 +0200

    Bytt til changard

--- init.sh
+++ init.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-echo "running init.sh script"
+set -eu
 
-if test -f /var/run/secrets/nais.io/serviceuser/username;
-then
-  export CREDENTIAL_USERNAME=$(cat /var/run/secrets/nais.io/serviceuser/username)
-  echo "- exporting CREDENTIAL_USERNAME"
-fi
+export CREDENTIAL_USERNAME="$(cat /var/run/secrets/nais.io/serviceuser/username)"
+export CREDENTIAL_PASSWORD="$(cat /var/run/secrets/nais.io/serviceuser/password)"
 
-if test -f /var/run/secrets/nais.io/serviceuser/password;
-then
-  export CREDENTIAL_PASSWORD=$(cat /var/run/secrets/nais.io/serviceuser/password)
-  echo "- exporting CREDENTIAL_PASSWORD"
-fi
+exec java -jar /app/app.jar
