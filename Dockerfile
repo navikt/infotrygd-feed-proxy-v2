1a4521a413a102b6c98973ea13e09c95af8d467d
Bjørn Erik Sannes <bjorn.erik.sannes@nav.no>
2026-09-16 13:30:54 +0200

    Bytt til changard (#212)
    * Bytt til changard
    
    * Bytt til changard

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM europe-north1-docker.pkg.dev/cgr-nav/pull-through/nav.no/jre:openjdk-26-dev
 ENV APP_NAME=infotrygd-feed-proxy-v2
 ENV JAVA_OPTS="-XX:MaxRAMPercentage=75"
 
-COPY init.sh /init-scripts/init.sh
-COPY ./target/app.jar .
+WORKDIR /app
 
-ENTRYPOINT ["java", "-jar", "app.jar"]
+COPY ./target/app.jar ./app.jar
+COPY --chown=nonroot:nonroot --chmod=0555 init.sh ./init.sh
+
+ENTRYPOINT ["/bin/sh", "/app/init.sh"]
--- init.sh
+++ init.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
-echo "running init.sh script"
+set -eu
 
-if test -f /var/run/secrets/nais.io/serviceuser/username;
-then
-  export CREDENTIAL_USERNAME=$(cat /var/run/secrets/nais.io/serviceuser/username)
-  echo "- exporting CREDENTIAL_USERNAME"
-fi
+CREDENTIAL_USERNAME="$(cat /var/run/secrets/nais.io/serviceuser/username)"
+CREDENTIAL_PASSWORD="$(cat /var/run/secrets/nais.io/serviceuser/password)"
+export CREDENTIAL_USERNAME CREDENTIAL_PASSWORD
 
-if test -f /var/run/secrets/nais.io/serviceuser/password;
-then
-  export CREDENTIAL_PASSWORD=$(cat /var/run/secrets/nais.io/serviceuser/password)
-  echo "- exporting CREDENTIAL_PASSWORD"
-fi
+exec java -jar /app/app.jar
